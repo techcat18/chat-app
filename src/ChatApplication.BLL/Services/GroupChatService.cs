ec092942e3f18e12cd38bd1d98ffb11879b824a7
Kostiantyn Bondarenko <techcat18@gmail.com>
2023-04-10 22:16:47 +0300

    Implemented the group chat service

--- src/ChatApplication.BLL/Services/GroupChatService.cs
+++ src/ChatApplication.BLL/Services/GroupChatService.cs
@@ -1,4 +1,6 @@
-﻿using ChatApplication.BLL.Models.GroupChat;
+﻿using AutoMapper;
+using ChatApplication.BLL.Exceptions.NotFound;
+using ChatApplication.BLL.Models.GroupChat;
 using ChatApplication.BLL.Services.Interfaces;
 using ChatApplication.DAL.Data.Interfaces;
 using ChatApplication.DAL.Entities;
@@ -8,34 +10,55 @@ namespace ChatApplication.BLL.Services;
 public class GroupChatService: IGroupChatService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public GroupChatService(IUnitOfWork unitOfWork)
+    public GroupChatService(
+        IUnitOfWork unitOfWork, 
+        IMapper mapper)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
-    public Task<IEnumerable<GroupChatModel>> GetAllAsync(CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<GroupChatModel>> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var groupChats = await _unitOfWork.GroupChatRepository.GetAllAsync(cancellationToken);
+        return _mapper.Map<IEnumerable<GroupChatModel>>(groupChats);
     }
 
-    public Task<GroupChatModel?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    public async Task<GroupChatModel?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var groupChat = await _unitOfWork.GroupChatRepository.GetByIdAsync(id, cancellationToken);
+        return _mapper.Map<GroupChatModel>(groupChat);
     }
 
-    public Task<GroupChatModel> CreateAsync(CreateGroupChatModel model, CancellationToken cancellationToken = default)
+    public async Task<GroupChatModel> CreateAsync(CreateGroupChatModel model, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var groupChat = _mapper.Map<GroupChat>(model);
+        
+        await _unitOfWork.GroupChatRepository.CreateAsync(groupChat, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        
+        return _mapper.Map<GroupChatModel>(groupChat);
     }
 
-    public Task UpdateAsync(UpdateGroupChatModel model, CancellationToken cancellationToken = default)
+    public async Task UpdateAsync(UpdateGroupChatModel model, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var groupChat = await _unitOfWork.GroupChatRepository.GetByIdAsync(model.Id, cancellationToken)
+                        ?? throw new GroupChatNotFoundException("Group chat was not found");
+
+        groupChat.Name = model.Name;
+
+        _unitOfWork.GroupChatRepository.Update(groupChat);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
     }
 
-    public Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var groupChat = await _unitOfWork.GroupChatRepository.GetByIdAsync(id, cancellationToken)
+                        ?? throw new GroupChatNotFoundException("Group chat was not found");
+
+        _unitOfWork.GroupChatRepository.Delete(groupChat);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
     }
 }
