e5c7ffa500b34d3f00e2088c321ff5c7bf647070
Kostiantyn Bondarenko <techcat18@gmail.com>
2023-04-10 21:51:26 +0300

    Added dependency registration for DAL layer

--- src/ChatApplication.DAL/DependencyRegistrar.cs
+++ src/ChatApplication.DAL/DependencyRegistrar.cs
@@ -1,6 +1,45 @@
-﻿namespace ChatApplication.DAL;
+﻿using ChatApplication.DAL.Data;
+using ChatApplication.DAL.Data.Interfaces;
+using ChatApplication.DAL.Repositories;
+using ChatApplication.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyRegistrar
+namespace ChatApplication.DAL;
+
+public static class DependencyRegistrar
 {
-    
+    public static IServiceCollection ConfigureDataAccessLayer(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.ConfigureRepositories();
+        services.ConfigureUnitOfWork();
+        services.ConfigureDbContext(configuration);
+
+        return services;
+    }
+
+    private static void ConfigureRepositories(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IGroupChatRepository, GroupChatRepository>();
+    }
+
+    private static void ConfigureUnitOfWork(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+
+    private static void ConfigureDbContext(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ChatDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SQLConnection"));
+        });
+    }
 }
