3d2b3e53a69dafa97eee2a81db8b34b6cc7a7f49
Kostiantyn Bondarenko <techcat18@gmail.com>
2023-05-05 12:57:28 +0300

    Refactoring

--- src/ChatApplication.API/Hubs/VideoHub.cs
+++ src/ChatApplication.API/Hubs/VideoHub.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using ChatApplication.BLL.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -7,6 +8,13 @@ namespace ChatApplication.API.Hubs;
 [Authorize(AuthenticationSchemes = "Bearer")]
 public class VideoHub: Hub
 {
+    private readonly IUserService _userService;
+
+    public VideoHub(IUserService userService)
+    {
+        _userService = userService;
+    }
+
     public override async Task OnConnectedAsync()
     {
         var userId = Context.User?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
@@ -25,15 +33,21 @@ public class VideoHub: Hub
         await Clients.OthersInGroup(chatId.ToString()).SendAsync("UserConnected", peerId, Context.ConnectionId);
     }
 
-    public async Task CallUserAsync(string peerId, string userId)
+    public async Task CallUserAsync(string peerId, string userId, string callerId)
     {
         var id = Context.User?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-        await Clients.Group(userId).SendAsync("AnswerCall", id);
+        var callerEmail = (await _userService.GetByIdAsync(callerId)).Email;
+        await Clients.Group(userId).SendAsync("AnswerCall", id, callerEmail);
     }
 
     public async Task AcceptUserCallAsync(string peerId, string userId)
     {
         await Clients.Group(userId).SendAsync("CallAccepted", peerId);
+    }
+
+    public async Task RejectUserCallAsync(string userId)
+    {
+        await Clients.Group(userId).SendAsync("CallRejected");
     }
 
     public async Task LeaveVideoCallAsync(int chatId)
